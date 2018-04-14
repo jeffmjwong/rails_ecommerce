b26cdd6bf6c153081052d1807197beef996197be
Jeff Wong <jeffmjwong@gmail.com>
2018-04-14 17:11:45 +1000

    Generate migration for users table with username column, setting uniqueness to true

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180414063608) do
+ActiveRecord::Schema.define(version: 20180414070326) do
 
   create_table "users", force: :cascade do |t|
     t.boolean "admin", default: false
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(version: 20180414063608) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
