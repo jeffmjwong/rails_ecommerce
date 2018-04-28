bd33e9331f53569a096cc29e9b87cfe27f60d458
Jeff Wong <jeffmjwong@gmail.com>
2018-04-28 15:58:47 +1000

    Calculate subtotal items and price automatically in users' cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,12 +2,23 @@ class CartsController < ApplicationController
 
   def show
     @cart = Cart.find(params[:id])
-    if !user_signed_in?
-      redirect_to root_path
-    elsif !current_user.admin && @cart.user != current_user
-      redirect_to root_path
-    end
-    @baskets = @cart.baskets
+    check_user
+    initialize_subtotal
   end
+
+  private
+
+    def check_user
+      if !user_signed_in?
+        redirect_to root_path
+      elsif !current_user.admin && @cart.user != current_user
+        redirect_to root_path
+      end
+    end
+
+    def initialize_subtotal
+      @subtotal_items = 0
+      @subtotal_price = 0
+    end
 
 end
