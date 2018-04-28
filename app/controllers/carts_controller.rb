5aeb26e74c81ee83121c44a1d931af6387c76135
Jeff Wong <jeffmjwong@gmail.com>
2018-04-28 21:55:25 +1000

    Minor bug fixes. Show out of stock if product quantity is 0

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,16 +3,23 @@ class CartsController < ApplicationController
   def show
     @cart = Cart.find(params[:id])
     check_user
+    check_quantity
     initialize_subtotal
   end
 
   private
 
     def check_user
-      if !user_signed_in?
+      if !user_signed_in? || (!current_user.admin && @cart.user != current_user)
         redirect_to root_path
-      elsif !current_user.admin && @cart.user != current_user
-        redirect_to root_path
+      end
+    end
+
+    def check_quantity
+      @cart.baskets.each do |basket|
+        if basket.product.quantity < 1
+          basket.update(quantity: 0)
+        end
       end
     end
 
