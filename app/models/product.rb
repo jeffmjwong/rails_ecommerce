ee7351bd23c84c47bba829707099ab5636714f89
Jeff Wong <jeffmjwong@gmail.com>
2018-04-30 21:15:25 +1000

    Refactor SCSS files. Style individual product show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   mount_uploader :photo, PhotoUploader
-  validates :name, presence: true, length: { minimum: 3, maximum: 120 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 200 }
   belongs_to :user
   has_many :baskets, dependent: :delete_all
 end
