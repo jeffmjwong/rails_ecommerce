3b2ac7a1f8e4ad764754cf624289d4b9c6464ef4
Jeff Wong <jeffmjwong@gmail.com>
2018-05-03 12:25:04 +1000

    Refactor SCSS and helper files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,25 @@
 module ApplicationHelper
 
   def username_link(user)
-    link_to user.username, user_path(user)
+    content_tag(:span, (link_to user.username, user_path(user)), class: "user-name")
   end
 
-  def product_photo_link(product)
-    link_to image_tag(product.photo_url(:homepage)), product_path(product)
+  def product_image_link(product)
+    content_tag :div, class: "product-image-container" do
+      link_to image_tag(product.photo_url(:homepage)), product_path(product)
+    end
   end
 
   def product_quantity(product)
-
+    if product.quantity > 0
+      content_tag(:span, product.quantity, class: "product-quantity")
+    else
+      content_tag(:span, "Out of stock", class: "product-oos")
+    end
   end
 
   def product_price(product)
-    sprintf('%.2f', product.unitprice)
+    content_tag(:span, "$#{sprintf('%.2f', product.unitprice)}", class: "product-price")
   end
 
 end
