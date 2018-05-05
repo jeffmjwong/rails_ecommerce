825ec91275bf98f93ad0156386459129edfdddf7
Jeff Wong <jeffmjwong@gmail.com>
2018-05-05 14:40:55 +1000

    Create and style search bar on nav bar

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -3,9 +3,7 @@ module NavbarHelper
   def show_shop(user)
     if user_signed_in?
       content_tag :p do
-        concat " | "
         concat link_to 'Shop', user_shop_path(user)
-        concat " | "
       end
     end
   end
@@ -13,9 +11,7 @@ module NavbarHelper
   def show_cart(user)
     if user_signed_in?
       content_tag :p do
-        concat " | "
         concat link_to 'Cart', cart_path(user.cart)
-        concat " | "
       end
     end
   end
@@ -23,7 +19,6 @@ module NavbarHelper
   def show_login_link(user)
     if user_signed_in?
       content_tag :p do
-        concat " | "
         concat link_to user.username, user_path(user)
         concat " ("
         concat link_to 'Logout', destroy_user_session_path, method: :delete
@@ -31,7 +26,6 @@ module NavbarHelper
       end
     else
       content_tag :p do
-        concat " | "
         concat content_tag(:span, link_to('Login', new_user_session_path), class: 'login')
         concat " - access shop & cart"
       end
